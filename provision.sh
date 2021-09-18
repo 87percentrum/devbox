7fa5fa29b19c3c2689b443d7e02fccb70a52f520
Umut <umut@yalcinkaya.ninja>
2021-09-18 10:52:25 +1000

    added composer and restart nginx/php

--- provision.sh
+++ provision.sh
@@ -1,2 +1,3 @@
 apt-get update && apt-get upgrade -qq
-apt-get install php7.2-fpm nginx mariadb-server nodejs -qq
+apt-get install php7.2-fpm nginx mariadb-server nodejs composer -qq
+service nginx restart && service php-fpm restart
